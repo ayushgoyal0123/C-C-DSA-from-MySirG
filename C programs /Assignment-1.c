42ea6f9c6eb4b826034583391eaea0b6c8e728c6
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-03-26 02:27:42 +0530

    Create Assignment-1.c

---
+++ C programs /Assignment-1.c
@@ -0,0 +1,95 @@
+//Q1
+// #include<stdio.h>
+// int main()
+// {
+//     printf("WelcomeStudents");
+//     return 0;
+// }
+
+//Q2
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Welcome\nStudents");
+//     return 0;
+// }
+
+//Q3 
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\"MySirg\"");
+//     return 0;
+// }
+
+//Q4
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\\n");
+//     return 0;
+// }
+
+//Q5
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\\r");
+//     return 0;
+// }
+
+//Q6
+// #include<stdio.h>
+// int main()
+// {
+//     printf("\"Teacher's Day\"");
+//     return 0;
+// }
+
+//Q7
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     printf("Enter 2 numbers to add: ");
+//     scanf("%d %d",&a,&b);
+//     printf("Sum of a and b is %d",a+b);
+//     return 0;
+// }
+
+
+//Q8
+// #include<stdio.h>
+// int main()
+// {
+//     int side;
+//     printf("Enter a side to calculate area odf Square: ");
+//     scanf("%d",&side);
+//     printf("Area of square is %d",side*side);
+//     return 0;
+// }
+
+// //Q9
+// #include<stdio.h>
+// int main()
+// {
+//     int length,breadth;
+//     printf("Enter length and breadth to calculate Area of rectangle(in cm): ");
+//     scanf("%d%d",&length,&breadth);
+//     printf("Area of rectangle is %d",length*breadth);
+//     return 0;
+// }
+
+//Q10
+#include<stdio.h>
+int main()
+{
+    int radius;
+    printf("Enter radius to calculate Area of a circle: ");
+    scanf("%d",&radius);
+    printf("Area of a circle is is %f",3.14*radius*radius);
+    return 0;
+}
+
+
